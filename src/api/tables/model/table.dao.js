870dd3ab92dce4f9c94f1687e9c845b7a15e95b2
Matheus Ashton <matheusashton@gmail.com>
2019-07-22 18:15:33 -0300

    adding max players restriction

--- src/api/tables/model/table.dao.js
+++ src/api/tables/model/table.dao.js
@@ -6,7 +6,7 @@ export const TableDao = {
   findAll: () => from(TableModel.find()),
   insert: (tableData) => from(TableModel.create(tableData)),
   addPlayer: ({ player, tableId }) => from(TableModel.updateOne(
-    { _id: tableId },
+    { _id: tableId, $where: 'this.players.length <= this.maxPlayers' },
     { $push: { players: player } }
   ))
 }
