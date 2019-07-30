410e6de2f2ca4fb5207e2794083e7bb9a34a0a98
Matheus Ashton <matheusashton@gmail.com>
2019-07-30 14:43:08 -0300

    adding cors middleware

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 import { httpListener } from '@marblejs/core'
 import { bodyParser$ } from '@marblejs/middleware-body'
+import { cors$ } from '@marblejs/middleware-cors'
 
 import api$ from 'api'
 
 const middlewares = [
-  bodyParser$()
+  bodyParser$(),
+  cors$({ origin: ['localhost:8080'] })
 ]
 
 const effects = [
