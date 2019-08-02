ef360e8ce79c0df0b0a25cda3fc924f5ffce7643
Matheus Ashton <matheusashton@gmail.com>
2019-08-02 15:13:43 -0300

    fixing cleanMongoData for 1 record only

--- src/api/common/lib/data.js
+++ src/api/common/lib/data.js
@@ -2,7 +2,7 @@ import { cond, is, map, not, and, T, identity } from 'ramda'
 
 const renameIdField = (_, { _id: id, ...others }) => ({ id: id, ...others })
 const mongoDataCleaning = model => model.toObject({ versionKey: false, transform: renameIdField })
-const isObject = and(is(Object), not(is(Array)))
+const isObject = subject => and(is(Object, subject), not(is(Array, subject)))
 
 export const cleanMongoData = cond([
   [is(Array), map(mongoDataCleaning)],
