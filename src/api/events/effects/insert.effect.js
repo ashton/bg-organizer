c256d70823b561029c6d3cba6537cf52039c4f13
Matheus Ashton <matheusashton@gmail.com>
2019-08-02 15:14:06 -0300

    returning event in response body

--- src/api/events/effects/insert.effect.js
+++ src/api/events/effects/insert.effect.js
@@ -1,13 +1,15 @@
 import { use } from '@marblejs/core'
-import { map, mergeMap, mapTo } from 'rxjs/operators'
+import { map, mergeMap } from 'rxjs/operators'
 
 import { EventDao } from '../model'
 import { insertEventValidator$ } from '../model/event.validators'
+import { cleanMongoData  } from '../../common/lib/data'
 
 export const insertEventEffect$ = req$ =>
   req$.pipe(
     use(insertEventValidator$),
     map(req => req.body),
     mergeMap(EventDao.insert),
-    mapTo({ status: 201 })
+    map(cleanMongoData),
+    map(event => ({  status: 201, body: event } ))
   )
