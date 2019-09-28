8ab062c1fccb5f2a9e3687fc3341220e9be242f7
Matheus Ashton Silva <matheusashton@gmail.com>
2019-09-28 00:00:08 -0300

    returning object in table create endponit

--- src/api/tables/effects/insert.effect.js
+++ src/api/tables/effects/insert.effect.js
@@ -1,14 +1,16 @@
 import { use } from '@marblejs/core'
-import { map, mergeMap, mapTo } from 'rxjs/operators'
+import { map, mergeMap } from 'rxjs/operators'
 import { assoc } from 'ramda'
 
 import { TableDao } from '../model'
 import { insertTableValidator$ } from '../model/table.validators'
+import { cleanMongoData  } from '../../common/lib/data'
 
 export const insertTableEffect$ = req$ =>
   req$.pipe(
     use(insertTableValidator$),
     map(req => assoc('event', req.params.eventId, req.body)),
     mergeMap(TableDao.insert),
-    mapTo({ status: 201 })
+    map(cleanMongoData),
+    map(table => ({ status: 201, body: table }) )
   )
